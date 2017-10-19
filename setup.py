5de5f6727ee1fb9cabb9c84a017803a83960e958
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-10-19 08:14:01 +0200

    Simplify tests

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@
 # Copyright 2016-2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from setuptools import Command, setup
+from setuptools import setup
 from setuptools import find_packages
-from unittest import TestLoader, TextTestRunner
 
 from os import path
 
@@ -55,36 +54,10 @@ if path.exists('requirements.txt'):
         install_requires = fh.read().splitlines()
 
 
-class FailTestException(Exception):
-    """ It provides a failing build """
-    pass
-
-
-class Tests(Command):
-    """Run test & coverage, using a default TextTestRunner."""
-
-    user_options = []  # < For Command API compatibility
-
-    def initialize_options(self, ):
-        pass
-
-    def finalize_options(self, ):
-        pass
-
-    def run(self, ):
-        loader = TestLoader()
-        tests = loader.discover('.', 'test_*.py')
-        t = TextTestRunner(verbosity=1)
-        res = t.run(tests)
-        if not res.wasSuccessful():
-            raise FailTestException()
-
-
 if __name__ == "__main__":
     setup(
         packages=find_packages(exclude=('tests')),
         use_scm_version=True,
-        cmdclass={'test': Tests},
         setup_requires=[
             'setuptools_scm',
         ],
